2b46c434d816cae459929ae6c19e0be697669358
Graeme Rocher <graeme.rocher@gmail.com>
2020-06-17 15:44:33 +0200

    Setup basic test harness

--- src/main/java/io/micronaut/gradle/MicronautExtension.java
+++ src/main/java/io/micronaut/gradle/MicronautExtension.java
@@ -2,6 +2,7 @@ package io.micronaut.gradle;
 
 import org.gradle.api.Action;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
 
 import javax.inject.Inject;
@@ -15,18 +16,38 @@ import javax.inject.Inject;
 public class MicronautExtension {
 
     private final Processing processing;
+    private final Property<String> micronautVersion;
 
     @Inject
     public MicronautExtension(ObjectFactory objectFactory) {
         this.processing = new Processing(objectFactory);
+        this.micronautVersion = objectFactory.property(String.class);
+    }
+
+    /**
+     * Configures the Micronaut version.
+     * @param version The micronaut version
+     * @return This extension
+     */
+    public MicronautExtension micronautVersion(String version) {
+        micronautVersion.set(version);
+        return this;
+    }
+
+    /**
+     * @return The micronaut version.
+     */
+    public Property<String> getMicronautVersion() {
+        return micronautVersion;
     }
 
     /**
      * Configures processing.
      * @param action The action
      */
-    public void processing(Action<AnnotationProcessingConfigBuilder> action) {
+    public MicronautExtension processing(Action<AnnotationProcessingConfigBuilder> action) {
         action.execute(processing);
+        return this;
     }
 
     /**
@@ -40,6 +61,13 @@ public class MicronautExtension {
      * Configures annotation processing.
      */
     interface AnnotationProcessingConfig {
+
+        /**
+         * Whether incremental processing is enabled.
+         * @return The module name
+         */
+        Property<Boolean> isIncremental();
+
         /**
          * The module name being processed
          * @return The module name
@@ -51,6 +79,12 @@ public class MicronautExtension {
          * @return The group name
          */
         Property<String> getGroup();
+
+        /**
+         * Annotation patterns to include in annotation processing.
+         * @return The annotation patterns
+         */
+        ListProperty<String> getAnnotations();
     }
 
     /**
@@ -58,25 +92,51 @@ public class MicronautExtension {
      */
     interface AnnotationProcessingConfigBuilder {
         /**
+         * Whether incremental processing is enabled.
+         * @param incremental True if incremental processing is enabled
+         * @return This builder
+         */
+        AnnotationProcessingConfigBuilder incremental(boolean incremental);
+
+        /**
          * The module name to use. Default to the gradle project name.
          * @param name The module name
+         * @return This builder
          */
-        void module(String name);
+        AnnotationProcessingConfigBuilder module(String name);
 
         /**
          * The group name being used. Defaults to the gradle group name.
          * @param name The group name
+         * @return This builder
          */
-        void group(String name);
+        AnnotationProcessingConfigBuilder group(String name);
+
+        /**
+         * The annotation patterns to include in processing.
+         * @param annotations The annotation patterns
+         * @return This builder
+         */
+        AnnotationProcessingConfigBuilder annotations(String...annotations);
     }
 
     private static class Processing implements AnnotationProcessingConfigBuilder, AnnotationProcessingConfig {
         private final Property<String> group;
+        private final Property<Boolean> incremental;
         private final Property<String> module;
+        private final ListProperty<String> annotations;
 
         Processing(ObjectFactory objectFactory) {
+            this.incremental = objectFactory.property(Boolean.class);
+            this.incremental.set(true);
             this.module = objectFactory.property(String.class);
             this.group = objectFactory.property(String.class);
+            this.annotations = objectFactory.listProperty(String.class);
+        }
+
+        @Override
+        public Property<Boolean> isIncremental() {
+            return incremental;
         }
 
         @Override
@@ -90,17 +150,42 @@ public class MicronautExtension {
         }
 
         @Override
-        public void module(String name) {
-            if (name != null) {
-                this.module.set(name);
-            }
+        public ListProperty<String> getAnnotations() {
+            return annotations;
         }
 
         @Override
-        public void group(String name) {
+        public AnnotationProcessingConfigBuilder incremental(boolean incremental) {
+            this.incremental.set(incremental);
+            return this;
+        }
+
+        @Override
+        public AnnotationProcessingConfigBuilder module(String name) {
+            if (name != null) {
+                this.module.set(name);
+            }
+            return this;
+        }
+
+        @Override
+        public AnnotationProcessingConfigBuilder group(String name) {
             if (name != null) {
                 this.group.set(name);
             }
+            return this;
+        }
+
+        @Override
+        public AnnotationProcessingConfigBuilder annotations(String... annotations) {
+            if (annotations != null && annotations.length > 0) {
+                for (String annotation : annotations) {
+                    if (annotation != null) {
+                        this.annotations.add(annotation);
+                    }
+                }
+            }
+            return this;
         }
     }
 }
---
+++ src/test/groovy/io/micronaut/gradle/MicronautLibraryPluginSpec.groovy
@@ -0,0 +1,60 @@
+package io.micronaut.gradle
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class MicronautLibraryPluginSpec extends Specification {
+
+    @Rule TemporaryFolder testProjectDir = new TemporaryFolder()
+
+    File settingsFile
+    File buildFile
+
+    def setup() {
+        settingsFile = testProjectDir.newFile('settings.gradle')
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def "test apply defaults for micronaut-library"() {
+        given:
+        settingsFile << "rootProject.name = 'hello-world'"
+        buildFile << """
+            plugins {
+                id "io.micronaut.micronaut-library"
+            }
+            
+            micronaut {
+                micronautVersion = "2.0.0.RC1"
+            }
+            
+            repositories {
+                jcenter()
+                mavenCentral()
+            }
+            
+        """
+        testProjectDir.newFolder("src", "main", "java", "example")
+        def javaFile = testProjectDir.newFile("src/main/java/example/Foo.java")
+        javaFile.parentFile.mkdirs()
+        javaFile << """
+package example;
+
+@javax.inject.Singleton
+class Foo {}
+"""
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('assemble')
+                .withPluginClasspath()
+                .build()
+
+        then:
+        result.task(":assemble").outcome == TaskOutcome.SUCCESS
+        result.output.contains("Creating bean classes for 1 type elements")
+    }
+}
