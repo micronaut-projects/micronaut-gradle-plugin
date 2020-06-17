da8b93ea8d51a64fdf36d969f48a28dafa99db13
Graeme Rocher <graeme.rocher@gmail.com>
2020-06-17 16:38:11 +0200

    Groovy support

--- src/test/groovy/io/micronaut/gradle/MicronautLibraryPluginSpec.groovy
+++ src/test/groovy/io/micronaut/gradle/MicronautLibraryPluginSpec.groovy
@@ -18,7 +18,7 @@ class MicronautLibraryPluginSpec extends Specification {
         buildFile = testProjectDir.newFile('build.gradle')
     }
 
-    def "test apply defaults for micronaut-library"() {
+    def "test apply defaults for micronaut-library and java"() {
         given:
         settingsFile << "rootProject.name = 'hello-world'"
         buildFile << """
@@ -56,5 +56,53 @@ class Foo {}
         then:
         result.task(":assemble").outcome == TaskOutcome.SUCCESS
         result.output.contains("Creating bean classes for 1 type elements")
+        new File(
+                testProjectDir.getRoot(),
+                'build/classes/java/main/example/$FooDefinition.class'
+        ).exists()
+    }
+
+    def "test apply defaults for micronaut-library and groovy"() {
+        given:
+        settingsFile << "rootProject.name = 'hello-world'"
+        buildFile << """
+            plugins {
+                id "io.micronaut.micronaut-library"
+                id "groovy"
+            }
+            
+            micronaut {
+                version "2.0.0.RC1"
+            }
+            
+            repositories {
+                jcenter()
+                mavenCentral()
+            }
+            
+        """
+        testProjectDir.newFolder("src", "main", "groovy", "example")
+        def javaFile = testProjectDir.newFile("src/main/groovy/example/Foo.groovy")
+        javaFile.parentFile.mkdirs()
+        javaFile << """
+package example;
+
+@javax.inject.Singleton
+class Foo {}
+"""
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('assemble')
+                .withPluginClasspath()
+                .build()
+
+        then:
+        result.task(":assemble").outcome == TaskOutcome.SUCCESS
+        new File(
+                testProjectDir.getRoot(),
+                'build/classes/groovy/main/example/$FooDefinition.class'
+        ).exists()
     }
 }
