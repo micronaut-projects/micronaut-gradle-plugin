47be17ff7b2d691283eea5f10ab492c2273bac8d
Graeme Rocher <graeme.rocher@gmail.com>
2020-06-17 18:38:08 +0200

    Configure runtime-osx dependency if on a Mac

--- src/main/java/io/micronaut/gradle/MicronautApplicationPlugin.java
+++ src/main/java/io/micronaut/gradle/MicronautApplicationPlugin.java
@@ -1,8 +1,12 @@
 package io.micronaut.gradle;
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
+import org.apache.tools.ant.taskdefs.condition.Os;
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.file.FileTree;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.TaskContainer;
 
@@ -16,17 +20,29 @@ import java.util.Map;
  * @since 1.0.0
  */
 public class MicronautApplicationPlugin extends MicronautLibraryPlugin {
+
+    public static final String CONFIGURATION_DEVELOPMENT_ONLY = "developmentOnly";
+
     @Override
     public void apply(Project project) {
         super.apply(project);
 
         Configuration developmentOnly = project
-                .getConfigurations().create("developmentOnly");
+                .getConfigurations().create(CONFIGURATION_DEVELOPMENT_ONLY);
 
 
+        if (Os.isFamily(Os.FAMILY_MAC)) {
+            project.getDependencies().add(CONFIGURATION_DEVELOPMENT_ONLY, "io.micronaut:micronaut-runtime-osx");
+        }
+        project.afterEvaluate(p -> {
+            final MicronautExtension ext = p.getExtensions().getByType(MicronautExtension.class);
+            final String v = getMicronautVersion(p, ext);
+            final DependencyHandler dependencyHandler = p.getDependencies();
+            dependencyHandler.add(CONFIGURATION_DEVELOPMENT_ONLY, dependencyHandler.platform("io.micronaut:micronaut-bom:" + v));
+        });
         final TaskContainer tasks = project.getTasks();
         tasks.withType(JavaExec.class, javaExec -> {
-            javaExec.getClasspath().plus(developmentOnly.getAsFileTree());
+            javaExec.classpath(developmentOnly);
             if (project.getGradle().getStartParameter().isContinuous()) {
                 Map<String, Object> sysProps = new LinkedHashMap<>();
                 sysProps.put("micronaut.io.watch.restart", true);
