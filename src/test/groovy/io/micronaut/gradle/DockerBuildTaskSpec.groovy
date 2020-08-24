64c9c80115c4944bd400062e018cf8aa08950289
Graeme Rocher <graeme.rocher@oracle.com>
2020-08-24 10:41:36 +0200

    Improve tests

--- src/test/groovy/io/micronaut/gradle/DockerBuildTaskSpec.groovy
+++ src/test/groovy/io/micronaut/gradle/DockerBuildTaskSpec.groovy
@@ -3,6 +3,7 @@ package io.micronaut.gradle
 
 import io.micronaut.gradle.docker.DockerSettings
 import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.BuildTask
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Rule
@@ -21,7 +22,7 @@ class DockerBuildTaskSpec extends Specification {
         buildFile = testProjectDir.newFile('build.gradle')
     }
 
-    def "test apply defaults for micronaut-library and java"() {
+    def "test build docker image"() {
         given:
         settingsFile << "rootProject.name = 'hello-world'"
         buildFile << """
@@ -31,12 +32,17 @@ class DockerBuildTaskSpec extends Specification {
             
             micronaut {
                 version "2.0.1"
+                
+                docker {
+                    tag "my-tag"
+                }
             }
             
             repositories {
                 jcenter()
                 mavenCentral()
             }
+            
             
             mainClassName="example.Application"
             
@@ -61,8 +67,61 @@ class Application {
                 .withPluginClasspath()
                 .build()
 
+        def task = result.task(":buildDockerImage")
         then:
-        result.task(":buildDockerImage").outcome == TaskOutcome.SUCCESS
+        result.output.contains("Successfully tagged my-tag:latest")
+        task.outcome == TaskOutcome.SUCCESS
+    }
+
+    def "test build docker native image"() {
+        given:
+        settingsFile << "rootProject.name = 'hello-world'"
+        buildFile << """
+            plugins {
+                id "io.micronaut.application"
+            }
+            
+            micronaut {
+                version "2.0.1"
+                
+                docker {
+                    tag "my-tag"
+                }
+            }
+            
+            repositories {
+                jcenter()
+                mavenCentral()
+            }
+            
+            
+            mainClassName="example.Application"
+            
+        """
+        testProjectDir.newFolder("src", "main", "java", "example")
+        def javaFile = testProjectDir.newFile("src/main/java/example/Application.java")
+        javaFile.parentFile.mkdirs()
+        javaFile << """
+package example;
+
+class Application {
+    public static void main(String... args) {
+    
+    }
+}
+"""
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('buildDockerNativeImage')
+                .withPluginClasspath()
+                .build()
+
+        def task = result.task(":buildDockerNativeImage")
+        then:
+        result.output.contains("Successfully tagged my-tag:latest")
+        task.outcome == TaskOutcome.SUCCESS
     }
 
 }
