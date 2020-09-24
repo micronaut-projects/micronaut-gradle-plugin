0898a4f196560dfde9bbb6e0cca0e8aed4b8c8dc
graemerocher <graeme.rocher@gmail.com>
2020-09-24 18:45:44 +0200

    Auto configure logging

--- src/main/java/io/micronaut/gradle/MicronautApplicationPlugin.java
+++ src/main/java/io/micronaut/gradle/MicronautApplicationPlugin.java
@@ -4,20 +4,17 @@ import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin;
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
 import io.micronaut.gradle.docker.MicronautDockerPlugin;
 import org.apache.tools.ant.taskdefs.condition.Os;
-import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.file.SourceDirectorySet;
-import org.gradle.api.plugins.*;
-import org.gradle.api.tasks.JavaExec;
-import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.SourceSetOutput;
-import org.gradle.api.tasks.TaskContainer;
-import org.gradle.api.tasks.testing.Test;
+import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.plugins.PluginContainer;
+import org.gradle.api.tasks.*;
 
+import java.io.File;
 import java.util.*;
 
 /**
@@ -48,6 +45,7 @@ public class MicronautApplicationPlugin extends MicronautLibraryPlugin {
             final MicronautExtension ext = p.getExtensions().getByType(MicronautExtension.class);
             final String v = getMicronautVersion(p, ext);
             final DependencyHandler dependencyHandler = p.getDependencies();
+            configureLogging(p, dependencyHandler);
             Dependency platform = resolveMicronautPlatform(dependencyHandler, v);
             dependencyHandler.add(CONFIGURATION_DEVELOPMENT_ONLY, platform);
 
@@ -60,7 +58,6 @@ public class MicronautApplicationPlugin extends MicronautLibraryPlugin {
                     )
                 );
             }
-            JavaApplication javaApplication = p.getExtensions().getByType(JavaApplication.class);
             micronautRuntime.getDependencies().forEach((scope, dependencies) -> {
                 for (String dependency : dependencies) {
                     dependencyHandler.add(scope, dependency);
@@ -94,9 +91,6 @@ public class MicronautApplicationPlugin extends MicronautLibraryPlugin {
                     });
                 });
 
-                // apply required GCP function dependencies
-                dependencyHandler.add(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME, "com.google.cloud.functions:functions-framework-api");
-                dependencyHandler.add(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME,  "io.micronaut.gcp:micronaut-gcp-function-http");
                 PluginContainer plugins = project.getPlugins();
                 // Google Cloud Function requires shadow packaging
                 if (!plugins.hasPlugin(ShadowPlugin.class)) {
@@ -129,6 +123,31 @@ public class MicronautApplicationPlugin extends MicronautLibraryPlugin {
         // all META-INF/services file into a single file otherwise this
         // will break the application
         tasks.withType(ShadowJar.class, ShadowJar::mergeServiceFiles);
+    }
+
+    private void configureLogging(Project p, DependencyHandler dependencyHandler) {
+        SourceSetContainer sourceSets = p.getConvention().getPlugin(JavaPluginConvention.class)
+                .getSourceSets();
+        SourceSet sourceSet = sourceSets.findByName("main");
+        if (sourceSet != null) {
+            SourceDirectorySet resources = sourceSet.getResources();
+            Set<File> srcDirs = resources.getSrcDirs();
+            for (File srcDir : srcDirs) {
+                if (new File(srcDir, "logback.xml").exists()) {
+                    dependencyHandler.add(
+                            sourceSet.getRuntimeOnlyConfigurationName(),
+                            "ch.qos.logback:logback-classic"
+                    );
+                    break;
+                } else if (new File(srcDir, "simplelogger.properties").exists()) {
+                    dependencyHandler.add(
+                            sourceSet.getRuntimeOnlyConfigurationName(),
+                            "org.slf4j:slf4j-simple"
+                    );
+                    break;
+                }
+            }
+        }
     }
 
     @Override
