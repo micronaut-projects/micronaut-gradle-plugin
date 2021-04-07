cb90aad6152b65335e3b74dddb1b05332527f7e3
Iván López <lopezi@objectcomputing.com>
2021-04-07 11:04:16 +0200

    Only change the main class for GraalVM when using AWS Lambda apps but not functions
    Fixes #171

--- src/test/groovy/io/micronaut/gradle/LambdaNativeImageSpec.groovy
+++ src/test/groovy/io/micronaut/gradle/LambdaNativeImageSpec.groovy
@@ -4,6 +4,7 @@ import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
+import spock.lang.Issue
 import spock.lang.Specification
 
 class LambdaNativeImageSpec extends Specification {
@@ -110,5 +111,60 @@ class LambdaNativeImageSpec extends Specification {
         and:
         dockerFileNative.find() { it.contains('my.own.main.class')}
         !dockerFileNative.find() { it.contains('com.example.Application')}
+    }
+
+    @Issue("https://github.com/micronaut-projects/micronaut-gradle-plugin/issues/171")
+    void 'mainclass is set correctly for an AWS Lambda function using custom-runtime and GraalVM'() {
+        given:
+        settingsFile << "rootProject.name = 'hello-world'"
+        buildFile << """
+            plugins {
+                id "io.micronaut.application"
+            }
+
+            micronaut {
+                version "2.3.4"
+                runtime "netty"
+            }
+
+            repositories {
+                mavenCentral()
+            }
+
+            dependencies {
+                implementation("io.micronaut:micronaut-validation")
+                implementation("io.micronaut:micronaut-runtime")
+                implementation("io.micronaut.aws:micronaut-function-aws")
+                implementation("io.micronaut.aws:micronaut-function-aws-custom-runtime")
+                runtimeOnly("ch.qos.logback:logback-classic")
+                testImplementation("io.micronaut:micronaut-http-client")
+            }
+
+            application {
+                mainClass.set("com.example.BookLambdaRuntime")
+            }
+
+            java {
+                sourceCompatibility = JavaVersion.toVersion('8')
+                targetCompatibility = JavaVersion.toVersion('8')
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments('dockerfileNative', '-Pmicronaut.runtime=lambda')
+            .withPluginClasspath()
+            .build()
+
+        def dockerfileNativeTask = result.task(':dockerfileNative')
+        def dockerFileNative = new File(testProjectDir.root, 'build/docker/DockerfileNative').readLines('UTF-8')
+
+        then:
+        dockerfileNativeTask.outcome == TaskOutcome.SUCCESS
+
+        and:
+        !dockerFileNative.find() { it.contains('-H:Class=io.micronaut.function.aws.runtime.MicronautLambdaRuntime')}
+        dockerFileNative.find() { it.contains('-H:Class=com.example.BookLambdaRuntime')}
     }
 }
