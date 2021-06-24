f6bfe80c3f8e7440ef46efe800eb0f7bd51d7f61
Cedric Champeau <cedric.champeau@gmail.com>
2021-06-24 17:29:54 +0200

    Generate the GraalVM resource file even without the Micronaut application plugin
    This commit adds support for generating the GraalVM resource-config.json file
    even if the Micronaut Application plugin isn't applied (e.g if the application
    plugin is).
    
    This basically adds some null checks on extensions, but ideally the plugin should
    react to different plugins being applied, and in particular the Micronaut
    application plugin.
    
    Fixes #223

--- src/test/groovy/io/micronaut/gradle/MicronautGraalPluginSpec.groovy
+++ src/test/groovy/io/micronaut/gradle/MicronautGraalPluginSpec.groovy
@@ -23,6 +23,30 @@ class MicronautGraalPluginSpec extends Specification {
 
     void 'generate GraalVM resource-config.json with OpenAPI and resources included'() {
         given:
+        withSwaggerMicronautApplication()
+
+        when:
+        def result = GradleRunner.create()
+            .withProjectDir(testProjectDir.root)
+            .withArguments('generateResourceConfigFile', '-i', '--stacktrace')
+            .withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":classes").outcome == TaskOutcome.SUCCESS
+        result.task(":generateResourceConfigFile").outcome == TaskOutcome.SUCCESS
+
+        and:
+        def resourceConfigFile = new File(testProjectDir.root, 'build/generated/resources/graalvm/resource-config.json')
+        def resourceConfigJson = new JsonSlurper().parse(resourceConfigFile)
+
+        resourceConfigJson.resources.pattern.any { it == "\\Qapplication.yml\\E" }
+        resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/app-0.0.yml\\E" }
+        resourceConfigJson.resources.pattern.any { it == "\\QMETA-INF/swagger/views/swagger-ui/index.html\\E" }
+    }
+
+    void 'generate GraalVM resource-config.json with OpenAPI and resources included without the Micronaut Application plugin'() {
+        given:
         withSwaggerApplication()
 
         when:
@@ -46,7 +70,28 @@ class MicronautGraalPluginSpec extends Specification {
     }
 
     @Requires({ GraalUtil.isGraalJVM() && !os.windows })
-    void 'native-image is called with the generated JSON file directory'() {
+    void 'native-image is called with the generated JSON file directory (Micronaut Application)'() {
+        given:
+        withSwaggerMicronautApplication()
+
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('nativeImage', '-i', '--stacktrace')
+                .withPluginClasspath()
+                .build()
+
+        then:
+        result.task(":classes").outcome == TaskOutcome.SUCCESS
+        result.task(":generateResourceConfigFile").outcome == TaskOutcome.SUCCESS
+        result.task(":nativeImage").outcome == TaskOutcome.SUCCESS
+
+        and:
+        result.output.contains("-H:ConfigurationFileDirectories=${new File(testProjectDir.root, 'build/generated/resources/graalvm').absolutePath}")
+    }
+
+    @Requires({ GraalUtil.isGraalJVM() && !os.windows })
+    void 'native-image is called with the generated JSON file directory (regular Application)'() {
         given:
         withSwaggerApplication()
 
@@ -66,7 +111,7 @@ class MicronautGraalPluginSpec extends Specification {
         result.output.contains("-H:ConfigurationFileDirectories=${new File(testProjectDir.root, 'build/generated/resources/graalvm').absolutePath}")
     }
 
-    private void withSwaggerApplication() {
+    private void withSwaggerMicronautApplication() {
         testProjectDir.newFile('openapi.properties') << 'swagger-ui.enabled=true'
         testProjectDir.newFolder('src', 'main', 'resources')
         testProjectDir.newFile('src/main/resources/application.yml') << 'micronaut.application.name: hello-world'
@@ -83,6 +128,51 @@ class MicronautGraalPluginSpec extends Specification {
             micronaut {
                 version "2.5.4"
             }
+            repositories {
+                mavenCentral()
+            }
+            group = "example.micronaut"
+            mainClassName="example.Application"
+        """
+        testProjectDir.newFolder("src", "main", "java", "example")
+        def javaFile = testProjectDir.newFile("src/main/java/example/Application.java")
+        javaFile.parentFile.mkdirs()
+        javaFile << """
+package example;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "app", version = "0.0"))
+@io.micronaut.core.annotation.Introspected
+class Application {
+    public static void main(String... args) {
+    }
+}
+"""
+    }
+
+    private void withSwaggerApplication() {
+        testProjectDir.newFile('openapi.properties') << 'swagger-ui.enabled=true'
+        testProjectDir.newFolder('src', 'main', 'resources')
+        testProjectDir.newFile('src/main/resources/application.yml') << 'micronaut.application.name: hello-world'
+        settingsFile << "rootProject.name = 'hello-world'"
+        String micronautVersion = '2.5.0'
+        buildFile << """
+            plugins {
+                id "application"
+                id "io.micronaut.graalvm"
+            }
+            dependencies {
+                annotationProcessor(enforcedPlatform("io.micronaut:micronaut-bom:$micronautVersion"))
+                annotationProcessor("io.micronaut:micronaut-inject-java")
+                annotationProcessor("io.micronaut.openapi:micronaut-openapi")
+                
+                implementation(enforcedPlatform("io.micronaut:micronaut-bom:$micronautVersion"))
+                implementation("io.micronaut:micronaut-inject")
+                implementation("io.swagger.core.v3:swagger-annotations")
+            }
+            
             repositories {
                 mavenCentral()
             }
