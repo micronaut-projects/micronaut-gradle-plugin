937b672b49724e10fc82af3fc005ba1334d8f2a9
Cédric Champeau <cedric.champeau@gmail.com>
2021-10-15 09:53:09 +0200

    Upgrade base docker image to Java 17 (#269)
    Fixes #267

--- src/main/java/io/micronaut/gradle/docker/MicronautDockerfile.java
+++ src/main/java/io/micronaut/gradle/docker/MicronautDockerfile.java
@@ -84,7 +84,7 @@ public class MicronautDockerfile extends Dockerfile implements DockerBuildOption
             case LAMBDA:
                 javaApplication.getMainClass().set("io.micronaut.function.aws.runtime.MicronautLambdaRuntime");
             default:
-                from(new Dockerfile.From(from != null ? from : "openjdk:16-alpine"));
+                from(new Dockerfile.From(from != null ? from : "openjdk:17-alpine"));
                 setupResources(this);
                 exposePort(exposedPorts);
                 getInstructions().addAll(additionalInstructions);
