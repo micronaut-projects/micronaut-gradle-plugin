a5f67f2992c619a5e1c0801a1b931ea9ee434846
Cedric Champeau <cedric.champeau@gmail.com>
2021-12-22 10:50:39 +0100

    Make it more convenient to declare AOT plugins
    A project like Micronaut Security can contribute new AOT modules.
    We need a user-friendly way to declare such extensions, and a way
    to configure them.
    
    This commit introduces an `aotPlugins` configuration which makes
    it more obvious where to declare the AOT extensions. It also adds
    a properties block to the AOT configuration, so that configuration
    keys from plugins can be used without resorting to a properties
    file.

--- src/test/groovy/io/micronaut/gradle/aot/BasicMicronautAOTSpec.groovy
+++ src/test/groovy/io/micronaut/gradle/aot/BasicMicronautAOTSpec.groovy
@@ -46,4 +46,61 @@ class BasicMicronautAOTSpec extends AbstractAOTPluginSpec {
         runtime << ['jit', 'native']
 
     }
+
+    def "can configure properties via the DSL #runtime"() {
+        withSample("aot/basic-app")
+        buildFile << """
+            micronaut {
+                aot {
+                    configurationProperties.putAll([
+                        'known.missing.types.enabled': 'false',
+                        'some.plugin.enabled': 'true'
+                    ])
+                }
+            }
+
+        """
+
+        when:
+        def result = build "prepare${runtime.capitalize()}Optimizations", "-i"
+
+        and: "configuration file is generated"
+        hasAOTConfiguration(runtime) {
+            withProperty('graalvm.config.enabled', 'native' == runtime ? 'true' : 'false')
+            withProperty('logback.xml.to.java.enabled', 'false')
+            withProperty('cached.environment.enabled', 'true')
+            withProperty('serviceloading.jit.enabled', 'true')
+            withProperty('serviceloading.native.enabled', 'true')
+            withProperty('yaml.to.java.config.enabled', 'true')
+            withProperty('scan.reactive.types.enabled', 'true')
+            withProperty('known.missing.types.enabled', 'false')
+            withProperty('sealed.property.source.enabled', 'true')
+            withProperty('precompute.environment.properties.enabled', 'true')
+            withProperty('deduce.environment.enabled', 'true')
+            withProperty('some.plugin.enabled', 'true')
+            withExtraPropertyKeys 'service.types', 'known.missing.types.list'
+        }
+
+        then: "Context configurer is loaded"
+        result.output.contains 'Java configurer loaded'
+
+        when:
+        interruptApplicationStartup()
+        result = build("optimizedRun")
+
+        then:
+        [
+                'io.micronaut.core.util.EnvironmentProperties',
+                'io.micronaut.core.async.publisher.PublishersOptimizations',
+                'io.micronaut.core.io.service.SoftServiceLoader$Optimizations',
+                'io.micronaut.context.env.ConstantPropertySources'
+        ].each {
+            assert result.output.contains("Setting optimizations for class $it")
+        }
+
+        where:
+        runtime << ['jit', 'native']
+
+    }
+
 }
