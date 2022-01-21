3bce4537bc006d684ff2cef974d013785e4766a4
Cedric Champeau <cedric.champeau@gmail.com>
2022-01-21 16:16:16 +0100

    Introduce a Micronaut Gradle module type
    This is the first step in splitting this project into multiple
    subprojects. It introduces a Micronaut Gradle plugin type, which
    is now applied on the root project, but will be moved to a
    subproject (then subprojects).

---
+++ buildSrc/src/main/groovy/io/micronaut/internal/build/plugin/MicronautPluginExtension.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2003-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.internal.build.plugin;
+
+import com.gradle.publish.PluginBundleExtension;
+import org.codehaus.groovy.runtime.StringGroovyMethods;
+import org.gradle.plugin.devel.GradlePluginDevelopmentExtension;
+
+import javax.inject.Inject;
+
+public abstract class MicronautPluginExtension {
+
+    private final PluginBundleExtension pluginBundle;
+    private final GradlePluginDevelopmentExtension gradlePlugin;
+
+    @Inject
+    public MicronautPluginExtension(PluginBundleExtension pluginBundle, GradlePluginDevelopmentExtension gradlePlugin) {
+        this.pluginBundle = pluginBundle;
+        this.gradlePlugin = gradlePlugin;
+    }
+
+    public void register(String alias, String pluginClass, String description) {
+        String[] parts = alias.split("\\.");
+        for (int i=1; i<parts.length; i++) {
+            parts[i] = StringGroovyMethods.capitalize(parts[i]);
+        }
+        String prettyName = String.join("", parts) + "Plugin";
+
+        gradlePlugin.getPlugins().create(prettyName, decl -> {
+            decl.setId("io.micronaut." + alias);
+            decl.setImplementationClass(pluginClass);
+            decl.setDescription(description);
+            decl.setDisplayName(description);
+        });
+
+        pluginBundle.getPlugins().named(prettyName, pluginConfig -> pluginConfig.setDisplayName(description));
+    }
+}
