02063818d89fa5b0ba75d11e895f0a469b798596
Cedric Champeau <cedric.champeau@gmail.com>
2022-03-28 17:17:15 +0200

    Get the AOT version from the catalog

---
+++ aot-plugin/src/templates/java/io/micronaut/gradle/aot/Versions.java
@@ -0,0 +1,8 @@
+package io.micronaut.gradle.aot;
+
+/**
+ * Versions needed at runtime by the AOT plugin.
+ */
+abstract class Versions {
+    static final String AOT_VERSION = "%MICRONAUT_AOT_VERSION%";
+}
