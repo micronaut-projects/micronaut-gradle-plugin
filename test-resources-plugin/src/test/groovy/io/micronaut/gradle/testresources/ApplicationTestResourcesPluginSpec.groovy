6a28137fd7e763c9e5cf78c5dfc79d4a3ed17404
Cedric Champeau <cedric.champeau@gmail.com>
2022-06-10 09:31:03 +0200

    Ignore test on Windows since Docker is not available

--- test-resources-plugin/src/test/groovy/io/micronaut/gradle/testresources/ApplicationTestResourcesPluginSpec.groovy
+++ test-resources-plugin/src/test/groovy/io/micronaut/gradle/testresources/ApplicationTestResourcesPluginSpec.groovy
@@ -2,7 +2,9 @@ package io.micronaut.gradle.testresources
 
 import io.micronaut.gradle.AbstractGradleBuildSpec
 import org.gradle.testkit.runner.TaskOutcome
+import spock.lang.IgnoreIf
 
+@IgnoreIf({ os.windows })
 class ApplicationTestResourcesPluginSpec extends AbstractGradleBuildSpec {
 
     def "integrates with test resources without further configuration"() {
