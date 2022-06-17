f1f750d83931d81ada0cef8ea077a398846177a1
Cédric Champeau <cedric.champeau@gmail.com>
2022-06-17 12:51:57 +0200

    Add custom `testResources` source set (#484)
    * Add custom `testResources` source set
    
    This commit adds a `testResources` source set when applying the
    test resources plugin. This makes it easy to implement custom
    test resources in a single project, by implementing the test
    resource resolver in `src/testResources/java` for example.
    
    The test resources declares in that source set are automatically
    added to the test resources classpath.
    
    * Introduce internal start task
    
    This fixes the logic for determining if the server should be kept alive after
    the build stops: now there's an internal start service task, which effectively
    starts the server. We determine if the server should be kept alive by looking
    if the task graph contains the _explicit_ task `startTestResourcesService`.

---
+++ test-resources-plugin/src/test/groovy/io/micronaut/gradle/testresources/CustomTestResourcePluginSpec.groovy
@@ -0,0 +1,21 @@
+package io.micronaut.gradle.testresources
+
+import io.micronaut.gradle.AbstractGradleBuildSpec
+import org.gradle.testkit.runner.TaskOutcome
+
+class CustomTestResourcePluginSpec extends AbstractGradleBuildSpec {
+
+    def "can implement a custom test resource using the testResources source set"() {
+        withSample("test-resources/custom-test-resource")
+
+        when:
+        def result = build 'test'
+
+        then:
+        result.task(':test').outcome == TaskOutcome.SUCCESS
+        result.output.contains "Loaded 2 test resources resolvers"
+        result.output.contains "demo.GreetingTestResource"
+        result.output.contains "io.micronaut.testresources.testcontainers.GenericTestContainerProvider"
+    }
+
+}
