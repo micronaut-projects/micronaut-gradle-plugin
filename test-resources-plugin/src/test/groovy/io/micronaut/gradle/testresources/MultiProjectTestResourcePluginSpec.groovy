5ab584a4158281035cba8d9633875044f701b7ac
Cedric Champeau <cedric.champeau@gmail.com>
2022-06-28 10:16:18 +0200

    Add test coverage for isolated multiprojects

--- test-resources-plugin/src/test/groovy/io/micronaut/gradle/testresources/MultiProjectTestResourcePluginSpec.groovy
+++ test-resources-plugin/src/test/groovy/io/micronaut/gradle/testresources/MultiProjectTestResourcePluginSpec.groovy
@@ -25,4 +25,22 @@ class MultiProjectTestResourcePluginSpec extends AbstractGradleBuildSpec {
         result.output.contains "io.micronaut.testresources.postgres.PostgreSQLTestResourceProvider"
     }
 
+    def "can use independent test resource services"() {
+        withSample("test-resources/isolated-multiproject")
+
+        when:
+        def result = build 'test'
+
+        then:
+        result.task(':app1:internalStartTestResourcesService').outcome == TaskOutcome.SUCCESS
+        result.task(':app1:test').outcome == TaskOutcome.SUCCESS
+        result.task(':app2:internalStartTestResourcesService').outcome == TaskOutcome.SUCCESS
+        result.task(':app2:test').outcome == TaskOutcome.SUCCESS
+        result.task(':app3:internalStartTestResourcesService').outcome == TaskOutcome.SUCCESS
+        result.task(':app3:test').outcome == TaskOutcome.SUCCESS
+        if (System.getenv("TESTCONTAINERS_RYUK_DISABLED") != "true") {
+            assert result.output.count("Creating container for image: testcontainers/ryuk") == 2
+        }
+    }
+
 }
