5d22fea191a3e88ccbc0aba085515e41b0f09fdb
Cédric Champeau <cedric.champeau@gmail.com>
2023-03-13 15:29:17 +0100

    Upgrade to latest build plugins (#669)
    * Upgrade to latest build plugins
    
    and fix tests after Micronaut validation changes
    
    * Use temurin for vendor

--- samples/aot/aws-function/src/main/java/example/micronaut/Book.java
+++ samples/aot/aws-function/src/main/java/example/micronaut/Book.java
@@ -1,7 +1,7 @@
 package example.micronaut;
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Introspected;
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Introspected
 public class Book {
