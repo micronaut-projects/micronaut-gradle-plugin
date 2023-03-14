e8aceaf177cc7cd0a93f50ecdc2ac31bc65c88e1
Cedric Champeau <cedric.champeau@gmail.com>
2023-03-14 14:46:49 +0100

    Fix test

--- functional-tests/src/test/groovy/io/micronaut/gradle/docker/DockerNativeFunctionalTest.groovy
+++ functional-tests/src/test/groovy/io/micronaut/gradle/docker/DockerNativeFunctionalTest.groovy
@@ -680,7 +680,7 @@ ENTRYPOINT ["java", "-jar", "/home/app/application.jar"]
 
         then:
         def dockerfileNative = new File(testProjectDir.root, 'build/docker/native-main/DockerfileNative').text
-        dockerfileNative == """FROM ghcr.io/graalvm/native-image:ol7-java17-22.3.0 AS graalvm
+        dockerfileNative == """FROM ghcr.io/graalvm/native-image:ol7-java11-22.3.0 AS graalvm
 WORKDIR /home/app
 COPY layers/libs /home/app/libs
 COPY server.iprof /home/app/server.iprof
