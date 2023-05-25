2fed303eb06bfba3b45510ae0e8cc38adbdb19e7
Tim Yates <tim.yates@gmail.com>
2023-05-25 12:08:10 +0100

    Unused import

--- crac-plugin/src/main/java/io/micronaut/gradle/crac/CRaCConfiguration.java
+++ crac-plugin/src/main/java/io/micronaut/gradle/crac/CRaCConfiguration.java
@@ -3,7 +3,6 @@ package io.micronaut.gradle.crac;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.PathSensitive;
 import org.gradle.api.tasks.PathSensitivity;
