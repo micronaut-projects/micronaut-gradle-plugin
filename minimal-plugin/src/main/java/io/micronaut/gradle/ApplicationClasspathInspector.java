cba8af3a65c8f8fb8e321a2cfabef4633bea4023
Gasper Kojek <gkojek@gradle.com>
2024-01-17 16:41:30 +0100

    Fixed imports

--- minimal-plugin/src/main/java/io/micronaut/gradle/ApplicationClasspathInspector.java
+++ minimal-plugin/src/main/java/io/micronaut/gradle/ApplicationClasspathInspector.java
@@ -18,7 +18,13 @@ package io.micronaut.gradle;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.tasks.*;
+import org.gradle.api.tasks.CacheableTask;
+import org.gradle.api.tasks.Classpath;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.PathSensitive;
+import org.gradle.api.tasks.PathSensitivity;
+import org.gradle.api.tasks.TaskAction;
 
 import java.io.File;
 import java.io.FileWriter;
