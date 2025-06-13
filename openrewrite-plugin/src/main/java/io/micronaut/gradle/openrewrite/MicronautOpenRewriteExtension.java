b903868d3846778f7b9c8b62d2a7fd051ce7eb57
issam liqali <liqaliissam@gmail.com>
2025-06-13 11:27:49 +0100

    remove annotations

--- openrewrite-plugin/src/main/java/io/micronaut/gradle/openrewrite/MicronautOpenRewriteExtension.java
+++ openrewrite-plugin/src/main/java/io/micronaut/gradle/openrewrite/MicronautOpenRewriteExtension.java
@@ -1,12 +1,8 @@
 package io.micronaut.gradle.openrewrite;
 
 import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.Optional;
 
 public interface MicronautOpenRewriteExtension {
 
-    @Input
-    @Optional
     Property<String> getVersion();
 }
