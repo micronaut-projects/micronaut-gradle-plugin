a10d1780ebdf4a1a3c8432c7454ba8044aa5183c
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-26 01:58:15 +0000

    Fix Kotlin functional test baselines

--- functional-tests/src/test/groovy/io/micronaut/gradle/kotlin/Kotest5FunctionalTest.groovy
+++ functional-tests/src/test/groovy/io/micronaut/gradle/kotlin/Kotest5FunctionalTest.groovy
@@ -54,13 +54,12 @@ class Kotest5FunctionalTest extends AbstractEagerConfiguringFunctionalTest {
                         |import io.kotest.core.spec.style.StringSpec
                         |
                         |@MicronautTest
-                        |class ExampleTest(private val application: EmbeddedApplication<*>): StringSpec() {
-                        |    init {
+                        |class ExampleTest(private val application: EmbeddedApplication<*>): StringSpec({
+                        |
                         |    "test the server is running" {
                         |        assert(application.isRunning)
                         |    }
-                        |    }
-                        |}
+                        |})
                         """.stripMargin()
         def configFile = testProjectDir.newFile("src/test/kotlin/example/ProjectConfig.kt")
         configFile << """package com.example
